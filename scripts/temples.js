4c9a0f336169424f792a86a78bdc724836d4d9e2
frdeleon-dev <chisco87@gmail.com>
2026-07-14 22:20:51 -0600

    first exercices week03

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,20 +1,20 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = document.lastModified;
 
-// 2. MENÚ HAMBURGUESA RESPONSIVO
+
 const mainNav = document.querySelector("nav");
 const hamburgerButton = document.querySelector("#menu");
 
-// Inicializar el símbolo de la hamburguesa
+
 if (hamburgerButton) {
     hamburgerButton.textContent = "☰";
 
-    // Evento de clic para abrir/cerrar el menú
+
     hamburgerButton.addEventListener("click", () => {
         mainNav.classList.toggle("open");
         hamburgerButton.classList.toggle("open");
 
-        // Cambiar el icono entre '☰' y '❌'
+
         if (hamburgerButton.classList.contains("open")) {
             hamburgerButton.textContent = "❌";
         } else {
