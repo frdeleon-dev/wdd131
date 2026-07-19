5c0ec3f3c64262e939b031f59abbde1339748523
frdeleon-dev <chisco87@gmail.com>
2026-07-18 22:06:48 -0600

    W03 Assignment: Country Page

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -1,2 +1,27 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+// Static var
+const temperature = 22; 
+const windSpeed = 10;
+
+
+const calculateWindChill = (temp, speed) =>(13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    let windChillDisplay = "N/A"; 
+
+    
+    if (temperature <= 10 && windSpeed > 4.8) {
+        windChillDisplay = `${calculateWindChill(temperature, windSpeed)} °C`;
+    }
+
+    
+    const windChillElement = document.getElementById("wind-chill");
+    if (windChillElement) {
+        windChillElement.textContent = windChillDisplay;
+    }
+
+
+});
