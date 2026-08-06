6154c4161ff1b9eb97abf5c8223e6143881320ba
frdeleon-dev <chisco87@gmail.com>
2026-08-05 23:36:48 -0600

    First one

---
+++ project/scripts/hamburger-menu.js
@@ -0,0 +1,20 @@
+const mainNav = document.querySelector("nav");
+const hamburgerButton = document.querySelector("#menu");
+
+
+if (hamburgerButton) {
+    hamburgerButton.textContent = "☰";
+
+
+    hamburgerButton.addEventListener("click", () => {
+        mainNav.classList.toggle("open");
+        hamburgerButton.classList.toggle("open");
+
+
+        if (hamburgerButton.classList.contains("open")) {
+            hamburgerButton.textContent = "❌";
+        } else {
+            hamburgerButton.textContent = "☰";
+        }
+    });
+}
